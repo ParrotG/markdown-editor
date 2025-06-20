052f4ef55cf2102458ae3b00e377a9eba3b256e5
ParrotG <gushucheng1@gmail.com>
2025-06-20 13:53:12 +0800

    markdown viewer v1

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
-<script setup lang="ts">
-
-</script>
-
 <template>
-  <div class="container mx-auto px-4">
-    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
-      <div class="bg-green-200 shadow p-4 rounded">Card 1</div>
-      <div class="bg-green-200 shadow p-4 rounded">Card 2</div>
-      <div class="bg-green-200 shadow p-4 rounded">Card 3</div>
-      <div class="bg-green-200 shadow p-4 rounded">Card 4</div>
-    </div>
+  <div class="max-w-prose mx-auto px-4 sm:px-6">
+      <MarkdownViewer :content="markdownSource" />
   </div>
+
 </template>
+
+<script setup lang="ts">
+import MarkdownViewer from './components/MarkdownViewer.vue'
+
+const markdownSource = `
+`
+</script>
 
 <style scoped>
 
---
+++ src/components/MarkdownViewer.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="prose prose-slate max-w-none" v-html="compiledHtml" />
+</template>
+
+<script lang="ts" setup>
+import { computed } from 'vue'
+import MarkdownIt from 'markdown-it'
+
+// Props
+const props = withDefaults(
+    defineProps<{
+      content?: string
+    }>(),
+    {
+      content: ''
+    }
+)
+
+
+// 实例化 MarkdownIt
+const md = new MarkdownIt({
+  html: true,
+  linkify: true,
+  typographer: true
+})
+
+// 计算后的 HTML 内容
+const compiledHtml = computed(() => md.render(props.content))
+
+</script>
+
+<style scoped>
+/* 可选：Tailwind Typography 插件增强 markdown 样式 */
+</style>
